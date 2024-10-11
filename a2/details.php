05668b726f234a7ac766cc6cb1df6b455e7e5366
reyantm <reyantelha@proton.me>
2024-10-11 11:23:43 +1100

    changes

--- a2/details.php
+++ a2/details.php
@@ -28,8 +28,8 @@ if (isset($_GET['id'])) {
 
     <main>
         <div class="details-content">
-            <div class="details-images">
-                <img src="<?php echo $row['images']; ?>" alt="<?php echo $row['caption']; ?>">
+            <div class="details-image">
+                <img src="images/<?php echo $row['image']; ?>" alt="<?php echo $row['caption']; ?>">
             </div>
             <div class="details">
             <div class="item">
