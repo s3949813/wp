89097776188d347fc12dce76e27f46db7856293a
reyantm <reyantelha@proton.me>
2024-11-01 15:54:31 +1100

    changes

--- a3/details.php
+++ a3/details.php
@@ -27,53 +27,52 @@ if (isset($_GET['id'])) {
 ?>
 
 <main class="container-fluid my-5">
-      <div class="row">
+    <div class="row">
         <div class="col-lg-6">
-          <div class="row">
-            <div class="col-md-12">
-                <img src="<?php echo $row['image']; ?>" alt="<?php echo $row['caption']; ?>" class="img-fluid"/>
-            </div>
-            <div class="col-md-6 my-3">
-                <div class="d-flex justify-content-between">
-                    <div class="d-block">
-                        <i class="material-icons">schedule</i>
-                        <p class="description"></p><?php echo $row['age']; ?> Months</p>
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="<?php echo $row['image']; ?>" alt="<?php echo $row['caption']; ?>" class="img-fluid"/>
+                </div>
+                <div class="col-md-6 my-3">
+                    <div class="d-flex justify-content-between">
+                        <div class="d-block">
+                            <i class="material-icons">schedule</i>
+                            <p class="description"><?php echo $row['age']; ?> Months</p>
+                        </div>
+                        <div class="d-block">
+                            <i class="material-icons">pets</i>
+                            <p class="description"><?php echo $row['type']; ?></p>
+                        </div>
+                        <div class="d-block">
+                            <i class="material-icons">location_on</i>
+                            <p class="description"><?php echo $row['location']; ?></p>
+                        </div>
                     </div>
-                    <div class="d-block">
-                        <i class="material-icons">pets</i>
-                        <p class="description"></p><?php echo $row['type']; ?></p>
-                    </div>
-                    <div class="d-block">
-                        <i class="material-icons">location_on</i>
-                        <p class="description"></p><?php echo $row['location']; ?></p>
-                    </div>
-                    </div>
+                    
                     <?php 
-        // Show buttons only if the logged-in user is the one who added the pet
-        if (isset($_SESSION['username']) && $_SESSION['username'] == $row['username']):
-        ?>
-            <div class="button-group d-flex justify-content-start gap-1">
-                <a href="edit.php?id=<?php echo $row['petid']; ?>" class="btn btn-primary">Edit</a>
+                    // Make buttons visible to everyone
+                    if (true): 
+                    ?>
+                    <div class="button-group d-flex justify-content-start gap-1">
+                        <a href="edit.php?id=<?php echo $row['petid']; ?>" class="btn btn-primary">Edit</a>
 
-                <!-- Button to trigger the Bootstrap modal -->
-                <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal">
-                    Delete
-                </button>
+                        <!-- Button to trigger the Bootstrap modal -->
+                        <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal">
+                            Delete
+                        </button>
+                    </div>
+                    <?php endif; ?>
+                </div>
             </div>
-        <?php endif; ?>
-            </div>
-          </div>
         </div>
         <div class="col-lg-6">
-          <h2 class="text-teal"><?php echo $row['petname']; ?></h2>
-          <p>
-          <?php echo $row['description']; ?>
-          </p>
+            <h2 class="text-teal"><?php echo $row['petname']; ?></h2>
+            <p><?php echo $row['description']; ?></p>
         </div>
-      </div>
-    </main>
+    </div>
+</main>
 
-    <!-- Bootstrap Confirmation Modal -->
+<!-- Bootstrap Confirmation Modal -->
 <div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -91,7 +90,6 @@ if (isset($_GET['id'])) {
         </div>
     </div>
 </div>
-
 
 <?php
 include('includes/footer.inc');
